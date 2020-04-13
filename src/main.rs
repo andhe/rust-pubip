8c8d82034b7f56747213cd9b298df81309e18c73
Andreas Henriksson <andreas@fatal.se>
2020-04-13 16:22:53 +0200

    Use ureq to get ip from https://ifconfig.co/ip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello World!");
+    let resp = ureq::get("https://ifconfig.co/ip").call();
+
+    // .ok() tells if response is 200-299.
+    if resp.ok() {
+        let mut ip = resp.into_string().unwrap();
+        println!("Response: {}", ip.trim());
+    } else {
+        println!("Failed to get ip.");
+    }
 }
