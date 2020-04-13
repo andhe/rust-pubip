cc1b2ed3bb28932f55750fb40dfac804521d77f6
Andreas Henriksson <andreas@fatal.se>
2020-04-13 17:16:16 +0200

    src: refactor ifconfig.co code to a function
    Return a struct with ip-address and provider to the main function.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-fn main() {
+struct PubIPResult {
+    provider: &'static str,
+    ip: String,
+}
+
+fn https_ifconfig_co() -> Result<PubIPResult, &'static str> {
     let resp = ureq::get("https://ifconfig.co/ip")
         .timeout_connect(1_000)
         .timeout_read(1_000)
@@ -8,8 +13,22 @@ fn main() {
     // .ok() tells if response is 200-299.
     if resp.ok() {
         let ip = resp.into_string().unwrap();
-        println!("Response: {}", ip.trim());
+
+        Ok(PubIPResult {
+            provider: "ifconfig.co (https)",
+            ip: String::from(ip.trim()),
+        })
     } else {
-        println!("Failed to get ip.");
+        Err("Request to ifconfig.co failed.")
+    }
+}
+
+fn main() {
+    if let Ok(pubip) = https_ifconfig_co() {
+        println!("PUBLIC_IP_PROVIDER=\"{}\"", pubip.provider);
+        println!("PUBLIC_IP={}", pubip.ip);
+    } else {
+        eprintln!("Failed to get ip.");
+        std::process::exit(1);
     }
 }
